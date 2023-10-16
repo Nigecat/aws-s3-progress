085c79b0448ec317968251f04fb07d3625521406
Nigecat <development@nigelq.com>
2023-10-16 11:52:33 +1100

    Update docs

--- examples/upload-file.rs
+++ examples/upload-file.rs
@@ -7,6 +7,7 @@ async fn main() -> Result<(), aws_sdk_s3::Error> {
     let config = aws_config::load_from_env().await;
     let client = aws_sdk_s3::Client::new(&config);
 
+    // NOTE: The frequency the callback is called at is determined by the chunk_size of the given ByteStream
     let body = ByteStream::default();
     let _response = client
         .put_object()
@@ -15,13 +16,11 @@ async fn main() -> Result<(), aws_sdk_s3::Error> {
         .body(body)
         // ----------------
         .customize() // internally, this function is a) not-async and b) always returns Ok(), so the next two lines are somewhat redundant
-        .await
-        .unwrap()
+        .await?
         .track(|chunk, current, total| println!("{current}/{total} ({chunk})"))
         // ----------------
         .send()
-        .await
-        .unwrap();
+        .await?;
 
     Ok(())
 }
